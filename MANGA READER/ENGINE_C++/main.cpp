76e68fc489ebd0e5b4b7b7c8990d8caf684d121f
MIKEY <109261767+mikeyoni@users.noreply.github.com>
2026-03-11 23:23:46 +0600

    yaah done

--- MANGA READER/ENGINE_C++/main.cpp
+++ MANGA READER/ENGINE_C++/main.cpp
@@ -1,18 +1,92 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <filesystem>
+#include <curl/curl.h>
+#include "json.hpp"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
+using namespace std;
+
+class webscpaer
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    public:
+    CURL* engine;
+    CURLcode error;
+    string html;
 
-    for (int i = 1; i <= 5; i++)
+    webscpaer() : engine(nullptr), error(CURLE_OK)
     {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+        curl_global_init(CURL_GLOBAL_ALL);
+        engine = curl_easy_init();
+
+        if (!engine)
+        {
+            cout<<" Faild to inishalized the engine ";
+        }
+
     }
 
+    static size_t write_data(void *content , size_t bites , size_t pakage , std::string *data)
+    {
+        size_t total_size = bites * pakage;
+        if (data)
+        {
+            data -> append(static_cast<char*>(content), total_size) ;
+            return total_size;
+        }
+        else
+        {
+            cout<<"\n\n\n DATA CURUPTION !!!\n\n";
+        }
+        return 0;
+
+    }
+
+    void scrapurl( const string &url)
+    {
+        html.clear();
+
+        if (engine)
+        {
+            curl_easy_setopt(engine, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(engine, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(engine, CURLOPT_WRITEDATA, &html);
+
+            error = curl_easy_perform(engine);
+
+        }
+
+        if (!error)
+        {
+            cout<<"\n\n\n SUCCESS !!!\n\n";
+            cout<<html;
+
+            html.clear();
+            curl_easy_cleanup(engine);
+            engine = nullptr;
+            curl_global_cleanup();
+        }
+        else
+        {
+            cout<<"\n\n\n FAILURE !!!\n\n" <<error<<endl;
+        }
+
+    }
+
+
+
+};
+
+int main()
+{
+    cout << "Hello World!!!" << endl;
+
+    webscpaer hi;
+
+    hi.scrapurl("https://www.google.com");
+
+
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
